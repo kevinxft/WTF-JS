eb75d0ee48c7f82fee543e1e1d9a60a01c5def81
kevinxft <kevinxft@gmail.com>
2023-05-15 18:06:42 +0800

    加入micro task

--- 实现Promise/index.js
+++ 实现Promise/index.js
@@ -1,7 +1,6 @@
-// 三个状态
 const PENDING = "pending";
-const FULFILLED = "fulfilled";
 const REJECTED = "rejected";
+const FULFILLED = "fulfilled";
 
 class MyPromise {
   #state = PENDING;
@@ -13,8 +12,8 @@ class MyPromise {
       this.#changeState(FULFILLED, data);
     };
 
-    const reject = (reason) => {
-      this.#changeState(REJECTED, reason);
+    const reject = (err) => {
+      this.#changeState(REJECTED, err);
     };
 
     try {
@@ -31,60 +30,80 @@ class MyPromise {
     return false;
   }
 
-  #changeState(state, result) {
+  #changeState(state, data) {
     if (this.#state !== PENDING) return;
     this.#state = state;
-    this.#result = result;
-    // this.#run();
+    this.#result = data;
+    this.#run();
+  }
+
+  #runMicroTask(func) {
+    if (
+      process !== null &&
+      typeof process === "object" &&
+      typeof process.nextTick === "function"
+    ) {
+      process.nextTick(func);
+    } else if (typeof MutationObserver === "function") {
+      const ob = new MutationObserver(func);
+      const textNode = document.createTextNode("1");
+      ob.observe(textNode);
+      textNode.data = "2";
+    } else {
+      setTimeout(func, 0);
+    }
   }
 
   #runOne(callback, resolve, reject) {
-    if (typeof callback !== "function") {
-      const settled = this.#state === FULFILLED ? resolve : reject;
-      settled(this.#result);
-      return;
-    }
-    try {
-      console.log(this.#result);
-      console.log(data);
-      const data = callback(this.#result);
-      if (this.#isPromiseLike(data)) {
-        data.then(resolve, reject);
-      } else {
-        resolve(data);
+    this.#runMicroTask(() => {
+      if (typeof callback !== "function") {
+        const settled = this.#state === FULFILLED ? resolve : reject;
+        settled(this.#result);
+        return;
       }
-    } catch (err) {
-      reject(err);
-    }
+
+      try {
+        const data = callback(this.#result);
+        if (this.#isPromiseLike(data)) {
+          data.then(resolve, reject);
+        } else {
+          resolve(data);
+        }
+      } catch (error) {
+        reject(error);
+      }
+    });
   }
 
   #run() {
     if (this.#state === PENDING) return;
     while (this.#handlers.length) {
-      const { onFulfilled, onRejected, resolve, reject } =
+      const { resolve, reject, onFulFilled, onRejected } =
         this.#handlers.shift();
       if (this.#state === FULFILLED) {
-        this.#runOne(onFulfilled, resolve, reject);
+        this.#runOne(onFulFilled, resolve, reject);
       } else {
         this.#runOne(onRejected, resolve, reject);
       }
     }
   }
 
-  then(onFulfilled, onRejected) {
+  then(onFulFilled, onRejected) {
     return new MyPromise((resolve, reject) => {
       this.#handlers.push({
-        onFulfilled,
-        onRejected,
         resolve,
         reject,
+        onFulFilled,
+        onRejected,
       });
       this.#run();
     });
   }
 }
 
-// test 1
-new MyPromise((resolve, reject) => {
-  resolve(1);
+// test1
+new MyPromise((resolve) => {
+  setTimeout(() => {
+    resolve(100);
+  }, 1000);
 }).then((data) => console.log(data));
